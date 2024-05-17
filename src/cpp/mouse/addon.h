3b4a45a012a56d0a2e9917503649a50b5377e5fa
Lawlzer <kevindaspam@gmail.com>
2024-05-16 20:09:22 -0500

    Remove all .h files -- they don't seem necessary?

--- src/cpp/mouse/addon.h
+++
@@ -1,8 +0,0 @@
-#ifndef MOUSE_H
-#define MOUSE_H
-
-#include <napi.h>
-
-Napi::Value getPosition(const Napi::CallbackInfo& info);
-
-#endif
--- src/cpp/screen/addon.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCREEN_H
-#define SCREEN_H
-
-#include <napi.h>
-
-Napi::Value GetWindowPixelsMain(const Napi::CallbackInfo& info);
-Napi::Value GetScreenPixelsMain(const Napi::CallbackInfo& info);
-
-#endif
