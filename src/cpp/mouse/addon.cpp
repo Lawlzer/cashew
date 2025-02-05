a3f5ae3317930d76adc0ee8e9d26fa7e4d9b2e51
Lawlzer <kevindaspam@gmail.com>
2025-02-04 23:37:07 -0600

    Improve mouse utility - Improve clicking consistency, remove "click" in favour of clickDesktop Implement "hold" and "release" mouse utilities. Improve Mouse related logging/debugging.

--- src/cpp/mouse/addon.cpp
+++ src/cpp/mouse/addon.cpp
@@ -19,24 +19,26 @@ struct rgb {
     unsigned int r, g, b;
 };
 
+/**
+ * Windows doesn't seem to allow you to click on background applications, without bringing them to the foregorund first.
+ */
 bool click(Napi::Env env, int x, int y, const string& windowTitle, int holdFor, int delayAfter, int clickCount, const string& button) {
     HWND hwnd = FindWindowA(nullptr, windowTitle.c_str());
-	if (!hwnd) {
-		throw Napi::TypeError::New(env, "Could not find the window: " + windowTitle);
-	}
-    
+    if (!hwnd) {
+        Napi::Error::New(env, "Could not find the window: " + windowTitle).ThrowAsJavaScriptException();
+        return false;
+    }
 
     POINT pt = {x, y};
     ClientToScreen(hwnd, &pt);
-    WPARAM wParam = (button == "right") ? MK_RBUTTON : MK_LBUTTON;
-    LPARAM lParam = MAKELPARAM(pt.x, pt.y);
-    UINT downMsg = (button == "right") ? WM_RBUTTONDOWN : WM_LBUTTONDOWN;
-    UINT upMsg = (button == "right") ? WM_RBUTTONUP : WM_LBUTTONUP;
+    DWORD downFlag = (button == "right") ? MOUSEEVENTF_RIGHTDOWN : MOUSEEVENTF_LEFTDOWN;
+    DWORD upFlag = (button == "right") ? MOUSEEVENTF_RIGHTUP : MOUSEEVENTF_LEFTUP;
     
     for (int i = 0; i < clickCount; i++) {
-        PostMessage(hwnd, downMsg, wParam, lParam);
+        SetCursorPos(pt.x, pt.y);
+        mouse_event(downFlag, 0, 0, 0, 0);
         std::this_thread::sleep_for(std::chrono::milliseconds(holdFor));
-        PostMessage(hwnd, upMsg, wParam, lParam);
+        mouse_event(upFlag, 0, 0, 0, 0);
         std::this_thread::sleep_for(std::chrono::milliseconds(delayAfter));
     }
     return true;
@@ -46,7 +48,7 @@ Napi::Value clickNapi(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
     if (info.Length() < 7) {
-        throw Napi::TypeError::New(env, "Expected 7 arguments");
+        Napi::Error::New(env, "Expected 7 arguments").ThrowAsJavaScriptException();
     }
 
     int x = info[0].As<Napi::Number>().Int32Value();
@@ -83,7 +85,7 @@ Napi::Value clickDesktopNapi(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
     if (info.Length() < 6) {
-        throw Napi::TypeError::New(env, "Expected 6 arguments");
+        Napi::Error::New(env, "Expected 6 arguments").ThrowAsJavaScriptException();
     }
 
     int x = info[0].As<Napi::Number>().Int32Value();
@@ -100,8 +102,21 @@ Napi::Value clickDesktopNapi(const Napi::CallbackInfo& info) {
 
 Napi::Value getPosition(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
+
+    if (info.Length() < 1) {
+        Napi::Error::New(env, "Expected 1 argument").ThrowAsJavaScriptException();
+    }
+
+    string windowTitle = info[0].As<Napi::String>().Utf8Value();
+    HWND hwnd = FindWindowA(nullptr, windowTitle.c_str());
+    if (!hwnd) {
+        Napi::Error::New(env, "Could not find the window: " + windowTitle).ThrowAsJavaScriptException();
+    }
+
     POINT p;
     if (GetCursorPos(&p)) {
+        // Convert screen coordinates to client coordinates for the specific window
+        ScreenToClient(hwnd, &p);
         Napi::Object result = Napi::Object::New(env);
         result.Set("x", Napi::Number::New(env, p.x));
         result.Set("y", Napi::Number::New(env, p.y));
@@ -111,10 +126,76 @@ Napi::Value getPosition(const Napi::CallbackInfo& info) {
     }
 }
 
+bool hold(Napi::Env env, int x, int y, const string& windowTitle, const string& button) {
+    HWND hwnd = FindWindowA(nullptr, windowTitle.c_str());
+    if (!hwnd) {
+        Napi::Error::New(env, "Could not find the window: " + windowTitle).ThrowAsJavaScriptException();
+    }
+
+    POINT pt = {x, y};
+    ClientToScreen(hwnd, &pt);
+    WPARAM wParam = (button == "right") ? MK_RBUTTON : MK_LBUTTON;
+    LPARAM lParam = MAKELPARAM(pt.x, pt.y);
+    UINT downMsg = (button == "right") ? WM_RBUTTONDOWN : WM_LBUTTONDOWN;
+    
+    PostMessage(hwnd, downMsg, wParam, lParam);
+    return true;
+}
+
+bool release(Napi::Env env, int x, int y, const string& windowTitle, const string& button) {
+    HWND hwnd = FindWindowA(nullptr, windowTitle.c_str());
+    if (!hwnd) {
+        Napi::Error::New(env, "Could not find the window: " + windowTitle).ThrowAsJavaScriptException();
+    }
+
+    POINT pt = {x, y};
+    ClientToScreen(hwnd, &pt);
+    WPARAM wParam = (button == "right") ? MK_RBUTTON : MK_LBUTTON;
+    LPARAM lParam = MAKELPARAM(pt.x, pt.y);
+    UINT upMsg = (button == "right") ? WM_RBUTTONUP : WM_LBUTTONUP;
+    
+    PostMessage(hwnd, upMsg, wParam, lParam);
+    return true;
+}
+
+Napi::Value holdNapi(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+
+    if (info.Length() < 4) {
+        Napi::Error::New(env, "Expected 4 arguments").ThrowAsJavaScriptException();
+    }
+
+    int x = info[0].As<Napi::Number>().Int32Value();
+    int y = info[1].As<Napi::Number>().Int32Value();
+    string windowTitle = info[2].As<Napi::String>().Utf8Value();
+    string button = info[3].As<Napi::String>().Utf8Value();
+
+    bool result = hold(env, x, y, windowTitle, button);
+    return Napi::Boolean::New(env, result);
+}
+
+Napi::Value releaseNapi(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+
+    if (info.Length() < 4) {
+        Napi::Error::New(env, "Expected 4 arguments").ThrowAsJavaScriptException();
+    }
+
+    int x = info[0].As<Napi::Number>().Int32Value();
+    int y = info[1].As<Napi::Number>().Int32Value();
+    string windowTitle = info[2].As<Napi::String>().Utf8Value();
+    string button = info[3].As<Napi::String>().Utf8Value();
+
+    bool result = release(env, x, y, windowTitle, button);
+    return Napi::Boolean::New(env, result);
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
     exports.Set(Napi::String::New(env, "click"), Napi::Function::New(env, clickNapi));
     exports.Set(Napi::String::New(env, "getPosition"), Napi::Function::New(env, getPosition));
     exports.Set(Napi::String::New(env, "clickDesktop"), Napi::Function::New(env, clickDesktopNapi));
+    exports.Set(Napi::String::New(env, "hold"), Napi::Function::New(env, holdNapi));
+    exports.Set(Napi::String::New(env, "release"), Napi::Function::New(env, releaseNapi));
     return exports;
 }
 
--- src/utils/mouse.ts
+++ src/utils/mouse.ts
@@ -6,17 +6,21 @@ import type { Position } from '.';
 import { Config } from './config';
 
 export class Mouse {
+	/**
+	 * WARNING: "holdFor" and "delayAfter" are ran in CPP, and are therefore blocking.
+	 *
+	 * This function is (probably long-term/forever) disabled, because Windows does not seem to allow you to click on background applications, without bringing them to the foreground first.
+	 */
 	public static async click(options: { button?: 'left' | 'right'; position: Position; clickCount?: number; holdFor?: number; delayAfter?: number; windowTitle?: string }): Promise<void> {
-		options.button ??= 'left';
-		options.clickCount ??= 1;
-		options.holdFor ??= 30;
-		options.delayAfter ??= 30;
-
-		const windowTitle = options?.windowTitle ?? Config.getProcessConfig().windowTitle ?? '';
-		await mouseAddon.click(options.position.x, options.position.y, windowTitle, options.holdFor, options.delayAfter, options.clickCount, options.button);
+		// options.button ??= 'left';
+		// options.clickCount ??= 1;
+		// options.holdFor ??= 30;
+		// options.delayAfter ??= 30;
+		// const windowTitle = options?.windowTitle ?? Config.getProcessConfig().windowTitle ?? '';
+		// await mouseAddon.click(options.position.x, options.position.y, windowTitle, options.holdFor, options.delayAfter, options.clickCount, options.button);
 	}
 
-	public static async clickDesktop(options: { button?: 'left' | 'right'; position: Position; clickCount?: number; holdFor?: number; delayAfter?: number; windowTitle?: string }): Promise<void> {
+	public static async clickDesktop(options: { button?: 'left' | 'right'; position: Position; clickCount?: number; holdFor?: number; delayAfter?: number }): Promise<void> {
 		options.button ??= 'left';
 		options.clickCount ??= 1;
 		options.holdFor ??= 30;
@@ -25,8 +29,27 @@ export class Mouse {
 		await mouseAddon.clickDesktop(options.position.x, options.position.y, options.holdFor, options.delayAfter, options.clickCount, options.button);
 	}
 
-	public static async getPosition(): Promise<Position> {
-		const windowTitle = '';
-		return mouseAddon.getPosition(windowTitle);
+	public static async getPosition(options?: { windowTitle?: string }): Promise<Position> {
+		const windowTitle = options?.windowTitle ?? Config.getProcessConfig().windowTitle ?? '';
+		const position = await mouseAddon.getPosition(windowTitle);
+
+		function isPosition(pos: any): pos is Position {
+			return typeof pos === 'object' && typeof pos.x === 'number' && typeof pos.y === 'number';
+		}
+
+		if (!isPosition(position)) throwError('position was not an object: ', position);
+		return position;
+	}
+
+	public static async hold(options: { button?: 'left' | 'right'; position: Position; windowTitle?: string }): Promise<void> {
+		options.button ??= 'left';
+		const windowTitle = options?.windowTitle ?? Config.getProcessConfig().windowTitle ?? '';
+		await mouseAddon.hold(options.position.x, options.position.y, windowTitle, options.button);
+	}
+
+	public static async release(options: { button?: 'left' | 'right'; position: Position; windowTitle?: string }): Promise<void> {
+		options.button ??= 'left';
+		const windowTitle = options?.windowTitle ?? Config.getProcessConfig().windowTitle ?? '';
+		await mouseAddon.release(options.position.x, options.position.y, windowTitle, options.button);
 	}
 }
